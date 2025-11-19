1f7f26b88c7732c77d9008d85d7d250112fe61c9
Dexied <33525453+Dexied@users.noreply.github.com>
2025-11-19 17:18:33 +0500

    570-000

--- Source/XVisionDemo.Target.cs
+++ Source/XVisionDemo.Target.cs
@@ -8,8 +8,9 @@ public class XVisionDemoTarget : TargetRules
 	public XVisionDemoTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+        DefaultBuildSettings = BuildSettingsVersion.V6;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 
-		ExtraModuleNames.AddRange( new string[] { "XVisionDemo" } );
+        ExtraModuleNames.AddRange( new string[] { "XVisionDemo" } );
 	}
 }
